bf744bdd5741c68505802d0eb0b834d63380a100
IvanMejico <ivan.mejico21@gmail.com>
2019-11-07 09:26:50 +0800

    changes on sending sensor data

--- Sending_Data/Sending_Data.ino
+++ Sending_Data/Sending_Data.ino
@@ -3,65 +3,30 @@
 #define TX 11
 String AP = "B315_E2741";       // CHANGE ME
 String PASS = "LEMNISCATE"; // CHANGE ME
-String HOST = "192.168.254.103";
+String HOST = "192.168.254.102";
 String PORT = "80";
-// String field = "field1";
-int countTrueCommand;
-int countTimeCommand; 
-boolean found = false; 
+
+unsigned long timeout_start_val;
 int valSensor = 1;
+
 SoftwareSerial esp8266(RX,TX);
- 
   
 void setup() {
   Serial.begin(9600);
   esp8266.begin(115200);
-  sendCommand("AT",5,"OK");
-  sendCommand("AT+CWMODE=1",5,"OK");
-  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+  sendCommand("AT",2000,"OK");
+  sendCommand("AT+CWMODE=1",2000,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",5000,"OK");
 }
 void loop() {
- valSensor = getSensorData();
- String getData = "GET /esp8266/wifidata.php?value="+String(valSensor);
- sendCommand("AT+CIPMUX=1",5,"OK");
- sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
- sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
- esp8266.println(getData);delay(1500);countTrueCommand++;
- sendCommand("AT+CIPCLOSE=0",5,"OK");
+  valSensor = getSensorData();
+  String getData = "GET /esp8266/wifidata.php?value="+String(valSensor);
+  sendCommand("AT+CIPMUX=1",2000,"OK");
+  sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,2000,"OK");
+  sendCommand("AT+CIPSEND=0," +String(getData.length()+4),5000,">");
+  esp8266.println(getData);delay(1500);
+  // sendCommand("AT+CIPCLOSE=0",2000,"OK");
 }
 int getSensorData(){
   return random(1000); // Replace with 
 }
-void sendCommand(String command, int maxTime, char readReplay[]) {
-  Serial.print(countTrueCommand);
-  Serial.print(". at command => ");
-  Serial.print(command);
-  Serial.print(" ");
-  while(countTimeCommand < (maxTime*1))
-  {
-    esp8266.println(command);//at+cipsend
-    if(esp8266.find(readReplay))//ok
-    {
-      found = true;
-      break;
-    }
-  
-    countTimeCommand++;
-  }
-  
-  if(found == true)
-  {
-    Serial.println("OYI");
-    countTrueCommand++;
-    countTimeCommand = 0;
-  }
-  
-  if(found == false)
-  {
-    Serial.println("Fail");
-    countTrueCommand = 0;
-    countTimeCommand = 0;
-  }
-  
-  found = false;
- }
