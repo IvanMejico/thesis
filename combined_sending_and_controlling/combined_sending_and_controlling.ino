244e520a4d402fe15410efb7b8395a003448d3f7
IvanMejico <ivan.mejico21@gmail.com>
2019-11-20 14:53:55 +0800

    Database Structure Changes (Production)

--- combined_sending_and_controlling/combined_sending_and_controlling.ino
+++ combined_sending_and_controlling/combined_sending_and_controlling.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
 #define ESP8266_RX 10  // Connect the TX pin from the ESP to this RX pin of the Arduino
 #define ESP8266_TX 11  // Connect the TX pin from the Arduino to the RX pin of ESP
@@ -7,10 +7,10 @@
 //NETWORK INFORMATION
 String SSID_ESP = "B315_E2741";         // WIFI SSID
 String SSID_KEY = "LEMNISCATE";         // WIFI PASSWORD
-String HOST = "192.168.254.101";        // HOST NAME (Raspberry Pi IP ord DNS)
+String HOST = "192.168.254.100";        // HOST NAME (Raspberry Pi IP ord DNS)
 String PORT = "80";
 
-String sensor_node_id = "ESN001";
+String sensor_node_id = "PSN004";
 
 String relay_url = "/sensordata/relay.php?";   // SCRIPT FROR RELAY CONTROL
 String send_url = "/sensordata/submit.php?";   // SCRIPT FOR SENDING SENSOR READINGS
@@ -34,7 +34,7 @@ const int numReadings = 10;   // Defines number of reading to calculate average 
 int readings[numReadings];    // the readings from the analog input
 
 
-SoftwareSerial esp8266(ESP8266_RX, ESP8266_TX);
+// SoftwareSerial esp8266(ESP8266_RX, ESP8266_TX);
 
 void setup() {
   // SETUP WIND SENSOR READING VALUES
@@ -44,8 +44,8 @@ void setup() {
 
   pinMode(relayPin, OUTPUT);
 
-  Serial.begin(9600);
-  esp8266.begin(115200);
+  Serial.begin(115200);
+  // esp8266.begin(115200);
   setupESP();
   digitalWrite(relayPin, LOW);
 }
@@ -54,11 +54,11 @@ void loop() {
   // checkRelay();   // check and set relay status from the server
 
   // for debugging
-  valSensor = random(5);
-  Serial.println(valSensor);
+  valSensor = random(10);
+  //Serial.println(valSensor);
 
   // valSensor = getSensorData();    // gather sensor dat
    sendSensorReading("current", valSensor);   // send sensor measurement to the server
   
-  Serial.println("---");
+  //Serial.println("---");
 }
--- combined_sending_and_controlling/sendSensorReading.ino
+++ combined_sending_and_controlling/sendSensorReading.ino
@@ -1,5 +1,5 @@
 void sendSensorReading(String unit, float reading) {
-    Serial.println("\n>> SENDING SENSOR READING...\n");
+    //Serial.println("\n>> SENDING SENSOR READING...\n");
     
     // SEND SENSOR DATA
     String getData = "GET " + send_url 
@@ -10,6 +10,6 @@ void sendSensorReading(String unit, float reading) {
     sendCommand("AT+CIPMUX=1",2000,"OK");
     sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,2000,"OK");
     sendCommand("AT+CIPSEND=0," +String(getData.length()+4),5000,">");
-    esp8266.println(getData);delay(1500);
+    Serial.println(getData);delay(1500);
     sendCommand("AT+CIPCLOSE=0",2000,"OK");
 }
