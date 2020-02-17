700a9333569ecb9cda61e2e0bf68c7468afc5205
IvanMejico <ivan.mejico21@gmail.com>
2020-02-17 10:03:17 +0800

    Some firmware changes.

--- environment_sensor_node/setRelay.ino
+++ environment_sensor_node/setRelay.ino
@@ -5,11 +5,15 @@ void setRelay(String relay_status) {
     secondCharacter = relay_status.substring(1,2);
 
     if(relay_status == "TR" || firstCharacter == "T" || secondCharacter == "R") {
-        digitalWrite(relayPin, HIGH);
-        Serial.println("Relay: HIGH");
+        if(digitalRead(relayPin) == LOW) {
+            digitalWrite(relayPin, HIGH);
+            Serial.println("Relay: HIGH");
+        }
     } else if (relay_status == "FL" || firstCharacter == "F" || secondCharacter == "L") {
-        Serial.println("Relay: LOW");
-        digitalWrite(relayPin, LOW);
+        if(digitalRead(relayPin) == HIGH) {
+            digitalWrite(relayPin, LOW);
+            Serial.println("Relay: LOW");
+        }
     } else
         Serial.println("Relay: Nothing happened");
 }
