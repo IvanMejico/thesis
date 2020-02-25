3deb44df5720c7c85b9a81bbd40531a13cceec5d
IvanMejico <ivan.mejico21@gmail.com>
2020-02-25 10:10:38 +0800

    Firmware changes. Added DC sensor measurements (production)

--- environment_sensor_node/serialDump.ino
+++ environment_sensor_node/serialDump.ino
@@ -1,10 +1,10 @@
 
 //pretty simple function - read everything out of the serial buffer and whats coming and get rid of it
 void serialDump(){
-    Serial.println("dump");
+    // Serial.println("dump");
   char temp;
-  while(esp8266.available()){
-    temp =esp8266.read();
+  while(Serial.available()){
+    temp =Serial.read();
     delay(1);//could play around with this value if buffer overflows are occuring
   }//while
   //Serial.println("DUMPED");
